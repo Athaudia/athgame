e68715e27d8958beb87202acbc704dc97e388cf3
Athaudia <athaudia@gmail.com>
2015-05-08 03:51:00 +0200

    changed to be a library, and changed build system to tup(todo: linux)

--- include/athgame.h
+++ include/athgame.h
@@ -1,4 +1,15 @@
 #pragma once
 
+#include "audio_region.h"
+#include "audio_stream.h"
+#include "events.h"
+#include "font.h"
+#include "gui.h"
+#include "platform.h"
+#include "state.h"
+#include "surface.h"
+#include "vec.h"
+#include "window.h"
+
 void ag_init();
 void ag_uninit();
