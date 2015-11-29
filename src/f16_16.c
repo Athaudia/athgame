8e90a997d4d9895148daaa70d20d2c911fd620b9
Athaudia <athaudia@gmail.com>
2015-11-29 12:40:02 +0100

    added f16_16__cos

--- src/f16_16.c
+++ src/f16_16.c
@@ -35,6 +35,14 @@ struct f16_16 f16_16__sin(struct f16_16 a)
 	return f16_16__mul(  f16_16__mul(f16_16__div(z, f16_16(2)), f16_16__sub(f16_16(3), f16_16__mul(z, z))),  f16_16(1-2*((a.val&0x8000) >> 15)));
 }
 
+struct f16_16 f16_16__cos(struct f16_16 a)
+{
+	a = f16_16__add(a, f16_16__div(f16_16(1), f16_16(2)));
+	struct f16_16 z = (struct f16_16){.val=(a.val&0x7fff)<<2};
+	z = f16_16__sub(z,f16_16(1));
+	return f16_16__mul(  f16_16__mul(f16_16__div(z, f16_16(2)), f16_16__sub(f16_16(3), f16_16__mul(z, z))),  f16_16(1-2*((a.val&0x8000) >> 15)));
+}
+
 double f16_16__to_double(struct f16_16 a)
 {
 	return a.integer + a.fractional/65536.0;
